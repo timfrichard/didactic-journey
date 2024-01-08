155665fcfac3b06fce8c88710fb96be5fa28d5d2
Tim Richard <tim.f.richard@gmail.com>
2024-01-08 04:30:42 -0600

    Initial upgrade of Spring Boot and other library frameworks.

--- purchase-order-system/src/test/java/com/richard/tim/purchase/order/system/service/DepartmentServiceIT.java
+++ purchase-order-system/src/test/java/com/richard/tim/purchase/order/system/service/DepartmentServiceIT.java
@@ -1,18 +1,18 @@
 package com.richard.tim.purchase.order.system.service;
 
 import com.richard.tim.purchase.order.system.model.entities.Department;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class DepartmentServiceIT {
 
